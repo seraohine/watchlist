68026fcbc5c0047b61dfe1f205f5fe97921eba6a
wx <wx375645615@gmail.com>
2024-01-01 12:24:44 +0800

    add static files

--- app.py
+++ app.py
@@ -2,20 +2,20 @@ from flask import Flask , render_template
 from markupsafe import escape
 app = Flask(__name__)
 
-name = 'Grey Li'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
+name = 'Star Wang'
+projects = [
+    {'title': 'Algorithm'},
+    {'title': 'Principles of computer composition'},
+    {'title': 'C++ Learning'},
+    {'title': 'Python Learning'},
+    {'title': 'Flask Learning'},
+    {'title': 'Tutorials To Making A OS'},
+    {'title': 'OS Learning'},
+    {'title': 'Django Learning'},
+    {'title': 'Qt Learning'},
+    {'title': 'Tutorials To Making A 2*2 Cube Solver By Python'},
 ]
 @app.route('/') # '/' is http://localhost:5000/
 def index():
-    return render_template('index.html',name = name , movies = movies)
+    return render_template('index.html',name = name , projects = projects)
 

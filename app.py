500eeffb25f83f5be07eb20a1639f922202c8426
wx <wx375645615@gmail.com>
2024-01-03 09:10:13 +0800

    modify the view and add the edit

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask , render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from markupsafe import escape
 import os
 import sys
 import click
@@ -12,54 +11,62 @@ else:
     prefix = 'sqlite:////'
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path,'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+
+class Projects(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    content = db.Column(db.Text)
+
+
 @app.cli.command()
-@click.option('--drop', is_flag = True ,  help = 'Create after drop.')
+@click.option('--drop', is_flag=True, help='Create after drop.')
 def initdb(drop):
-    if drop:            #init the database
+    if drop:  # init the database
         db.drop_all()
     db.create_all()
     click.echo('Initialized database')
 
 
-
-name = 'Star Wang'
-projects = [
-    {'title': 'Algorithm'},
-    {'title': 'Principles of computer composition'},
-    {'title': 'C++ Learning'},
-    {'title': 'Python Learning'},
-    {'title': 'Flask Learning'},
-    {'title': 'Tutorials To Making A OS'},
-    {'title': 'OS Learning'},
-    {'title': 'Django Learning'},
-    {'title': 'Qt Learning'},
-    {'title': 'Tutorials To Making A 2*2 Cube Solver By Python'},
-]
-
-class User(db.Model):
-    id = db.Column(db.Integer , primary_key = True)
-    name = db.Column(db.String(20))
-
-class Projects(db.Model):
-    id = db.Column(db.Integer , primary_key = True)
-    title = db.Column(db.String(60))
-
-
 @app.context_processor
-def inject_user():  # 函数名可以随意修改
+def inject_user():
     user = User.query.first()
-    return dict(user=user)  # 需要返回字典，等同于 return {'user': user}
+    return dict(user=user)
 
 
-@app.route('/') # '/' is http://localhost:5000/
+@app.route('/')
 def index():
-    project = Projects.query.all()
-    return render_template('index.html', projects = projects)
+    projects = Projects.query.all()
+    return render_template('index.html', projects=projects)
 
-@app.errorhandler(404)  # 传入要处理的错误代码
-def page_not_found(e):  # 接受异常对象作为参数
-    return render_template('404.html'), 404  # 返回模板和状态码
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.route('/edit/<int:project_id>', methods=['GET', 'POST'])
+def edit_project(project_id):
+    project = Projects.query.get_or_404(project_id)
+
+    if request.method == 'POST':
+        project.content = request.form['content']
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return render_template('edit_project.html', project=project)
+
+
+@app.route('/view/<int:project_id>')
+def view_project(project_id):
+    project = Projects.query.get_or_404(project_id)
+    return render_template('view_project.html', project=project)
+
